b4b67a81a25027fb710c7ead4e5e136e25a6b724
imwaykul <imwaykul@gmail.com>
2018-09-27 13:07:54 -0400

    Add files via upload

---
+++ GreetingsDetective.py
@@ -0,0 +1,10 @@
+import pysttx3
+
+engine = pysttx3.init()
+engine.say("Greetings Detective, I can now confide in you regarding what I saw 2 weeks back")
+engine.runAndWait()
+engine.say("The group that you are going after is called 'Civilian Insurgency'. They are people from all different walks of life, all somehow connected.")
+engine.runAndWait()
+engine.say("I assume you heard rumors about how this group has several rendezvous points. In fact, they change up rendezvous points every few weeks.")
+engine.runAndWait()
+engine.say("I've narrowed down the leader to six suspects: Jeoffrey Times, Alexander Baker, Ronald Williams, Patrice O'Mara , Allison Williams, and Selena Goldsman")
